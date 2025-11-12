9de927e69e92212e467ca85b7637ddb626a2f988
lyhduc <lyhongduc.34@gmail.com>
2025-11-12 18:16:50 +0700

    fix(redis-modules): config fix

--- src/modules/redis/redis.module.ts
+++ src/modules/redis/redis.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { RedisService } from './redis.service';
 import { RedisPubsubService } from './redis-pubsub/redis-pubsub.service';
 import { RedisCacheService } from './redis-cache/redis-cache.service';
+import { redisConfig } from '../../config/redis.config';
 
 @Module({
+  imports: [ConfigModule.forFeature(redisConfig)],
   providers: [RedisService, RedisCacheService, RedisPubsubService],
   exports: [RedisService, RedisCacheService, RedisPubsubService],
 })
